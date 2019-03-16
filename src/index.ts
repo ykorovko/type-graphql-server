098915b3a164484e6a80800f5c872ac4d76d7280
Yura Korovko <ykorovko@gmail.com>
2019-03-16 14:12:27 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import * as Express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema, Resolver, Query } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello world!!!";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver]
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on http://localhost:4000/graphql");
+  });
+};
+
+main();
